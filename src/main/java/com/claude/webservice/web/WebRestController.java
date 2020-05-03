79901b383ae9b984f782ee604f3e31dd41713d6f
claudekim52 <javadeveloper@kakao.com>
2020-05-03 13:19:04 +0900

    503

--- src/main/java/com/claude/webservice/web/WebRestController.java
+++ src/main/java/com/claude/webservice/web/WebRestController.java
@@ -4,18 +4,21 @@ import com.claude.webservice.domain.posts.PostsRepository;
 import com.claude.webservice.dto.posts.PostsSaveRequestDto;
 import com.claude.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     // AllArgsConstruct가 빈을 주입해줌
-    private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("hello")
     public String hello() {
@@ -25,5 +28,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
