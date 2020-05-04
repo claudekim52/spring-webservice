ecb793c20fb50cf2b2372a8d497f082b1ae23c0c
claudekim52 <javadeveloper@kakao.com>
2020-05-04 16:17:43 +0900

    db

--- src/test/java/com/claude/webservice/web/WebControllerTest.java
+++ src/test/java/com/claude/webservice/web/WebControllerTest.java
@@ -1,6 +1,6 @@
 package com.claude.webservice.web;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class WebControllerTest {
+public class WebControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
